69cf26e8ca0d82961685353939f531949b6c45d1
Jimmy Zheng <jimmyz0622@gmail.com>
2021-08-20 15:42:16 -0700

    goes through each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,97 @@ var endScreen = document.querySelector("#end-screen");
 var highscoreScreen = document.querySelector("#highscore-screen");
 
 var timeEl = document.querySelector("#time");
-var timeLeft = 2; //75
+var timeLeft = 75; //75
 
 var initials = document.querySelector("#initials");
 
-function countdown() {
-    var timeInterval = setInterval(function() {
+var questionEl = document.querySelector("#question");
+var option1El = document.querySelector("#option1");
+var option2El = document.querySelector("#option2");
+var option3El = document.querySelector("#option3");
+var option4El = document.querySelector("#option4");
+
+const questions = [
+    {
+        question: "blah",
+        options: ["a", "s", "d", "f"],
+        correctAnswer: "a"
+    },
+    {
+        question: "blah2",
+        options: ["a", "s", "d", "f"],
+        correctAnswer: "s"
+    }
+]
+
+var questionNumber = 0;
+var score = 0;
+
+function showQuestion() {
+    if (questionNumber < questions.length) {
+        questionEl.textContent = questions[questionNumber].question;
+        option1El.value = questions[questionNumber].options[0];
+        option2El.value = questions[questionNumber].options[1];
+        option3El.value = questions[questionNumber].options[2];
+        option4El.value = questions[questionNumber].options[3];
+    }
+}
+
+option1El.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (option1El.value == questions[questionNumber].correctAnswer) {
+        score++;
+        questionNumber++;
+        showQuestion();
+    }
+    else {
+        timeLeft = timeLeft - 10;
+    }
+});
+
+option2El.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (option2El.value === questions[questionNumber].correctAnswer) {
+        score++;
+        questionNumber++;
+        showQuestion();
+    }
+    else {
+        timeLeft = timeLeft - 10;
+    }
+});
+
+option3El.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (option3El.value === questions[questionNumber].correctAnswer) {
+        score++;
+        questionNumber++;
+        showQuestion();
+    }
+    else {
+        timeLeft = timeLeft - 10;
+    }
+});
+
+option4El.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (option4El.value === questions[questionNumber].correctAnswer) {
+        score++;
+        questionNumber++;
+        showQuestion();
+    }
+    else {
+        timeLeft = timeLeft - 10;
+    }
+});
+
+function startQuiz() {
+    showQuestion();
+    var timeInterval = setInterval(function () {
         timeLeft--;
         timeEl.textContent = timeLeft;
 
-        if (timeLeft === 0) {
+        if (timeLeft <= 0 || questionNumber >= questions.length) {
             clearInterval(timeInterval);
             timeEl.textContent = "";
             questionScren.style.display = "none";
@@ -28,16 +109,16 @@ function countdown() {
     }, 1000);
 }
 
-startButton.addEventListener("click", function(event) {
+startButton.addEventListener("click", function (event) {
     event.preventDefault();
     startScreen.style.display = "none";
     questionScren.style.display = "block";
-    countdown();
+    startQuiz();
 });
 
-submitButton.addEventListener("click", function(event) {
+submitButton.addEventListener("click", function (event) {
     event.preventDefault();
-    
+
     var userScore = {
         initials: initials.value,
         score: 0
@@ -46,13 +127,13 @@ submitButton.addEventListener("click", function(event) {
     localStorage.setItem("userScore", JSON.stringify(userScore));
 });
 
-goBackButton.addEventListener("click", function(event) {
+goBackButton.addEventListener("click", function (event) {
     event.preventDefault();
     highscoreScreen.style.display = "none";
     startScreen.style.display = "block";
 })
 
-viewHighscore.addEventListener("click", function(event) {
+viewHighscore.addEventListener("click", function (event) {
     event.preventDefault();
     startScreen.style.display = "none";
     questionScren.style.display = "none";
