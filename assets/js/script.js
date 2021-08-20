207d9338a9b58c0b52b265a9c91cdbc9978f5d77
Jimmy Zheng <jimmyz0622@gmail.com>
2021-08-20 13:54:28 -0700

    button function and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,50 @@ var startButton = document.querySelector("#start");
 var submitButton = document.querySelector("#submit");
 var goBackButton = document.querySelector("#go-back");
 var clearButton = document.querySelector("#clear-highscore");
+var viewHighscore = document.querySelector("#view-highscore");
 
-startButton.addEventListener("click", function() {
-    
+var startScreen = document.querySelector("#start-screen");
+var questionScren = document.querySelector("#question-screen");
+var endScreen = document.querySelector("#end-screen");
+var highscoreScreen = document.querySelector("#highscore-screen");
+
+var timeEl = document.querySelector("#time");
+var timeLeft = 75;
+
+function countdown() {
+    var timeInterval = setInterval(function() {
+        timeLeft--;
+        timeEl.textContent = timeLeft;
+
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            timeEl.textContent = "";
+            questionScren.style.display = "none";
+            endScreen.style.display = "block";
+        }
+    }, 1000);
+}
+
+startButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    startScreen.style.display = "none";
+    questionScren.style.display = "block";
+    countdown();
 });
+
+goBackButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    highscoreScreen.style.display = "none";
+    startScreen.style.display = "block";
+})
+
+viewHighscore.addEventListener("click", function(event) {
+    event.preventDefault();
+    startScreen.style.display = "none";
+    questionScren.style.display = "none";
+    endScreen.style.display = "none";
+    highscoreScreen.style.display = "block";
+});
+
+
+
