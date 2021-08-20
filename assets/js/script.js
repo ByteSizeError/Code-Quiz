7b61c96314ee4ff645dc25b36b719e6a2153c433
Jimmy Zheng <jimmyz0622@gmail.com>
2021-08-19 20:58:37 -0700

    buttons added to js file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var startButton = document.querySelector("#start");
+var submitButton = document.querySelector("#submit");
+var goBackButton = document.querySelector("#go-back");
+var clearButton = document.querySelector("#clear-highscore");
+
+startButton.addEventListener("click", function() {
+    
+});
